41d8bbce7c14e25e863a4a9bc9781fdc1edb9abb
Tobias Pöppke <t.poeppke@gmx.de>
2017-05-31 01:12:58 +0200

    Bump version

--- org.palladiosimulator.pcm.cloud/src/main/java/org/palladiosimulator/pcm/cloud/pcmcloud/resourceenvironmentcloud/ResourceenvironmentcloudPackage.java
+++ org.palladiosimulator.pcm.cloud/src/main/java/org/palladiosimulator/pcm/cloud/pcmcloud/resourceenvironmentcloud/ResourceenvironmentcloudPackage.java
@@ -43,7 +43,7 @@ public interface ResourceenvironmentcloudPackage extends EPackage {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	String eNS_URI = "http://palladiosimulator.org/PalladioComponentModel/5.1/Cloud/ResourceEnvironment";
+	String eNS_URI = "http://palladiosimulator.org/PalladioComponentModel/5.2/Cloud/ResourceEnvironment";
 
 	/**
 	 * The package namespace name.
